806d2927e199766a6c36fd1dbc214e4b144486e1
AlbertTorres <tipetin@gmail.com>
2025-08-07 18:07:08 -0500

    Se subio la cantidad de numeros
    Hice cambios en la cantidad de numeros disponibles para adivinar

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const readline = require('readline');
 
-// Número aleatorio entre 1 y 100 en donde la idea es que le jugador intente adivinar el numero con pistas de frio y caliente
+// Número aleatorio entre 1 y 150 en donde la idea es que le jugador intente adivinar el numero con pistas de frio y caliente
 // No se que mas poner para los cambios pero esta curioso la verdad :D
-const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+const numeroSecreto = Math.floor(Math.random() * 150) + 1;
 
 const rl = readline.createInterface({
   input: process.stdin,
